565870b05db3db633f3bb12fcd8f4c61281f01f7
Talha Khalid <talhakhalid3324@gmail.com>
2020-09-21 08:00:25 -0700

    Updates

---
+++ precache-manifest.5cd4756108acf3fa0b9b494403492c99.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f749b9628500a897e5ca46de0cba97b",
+    "url": "/shadhinMusicApp/index.html"
+  },
+  {
+    "revision": "f05e532cf51e8458d118",
+    "url": "/shadhinMusicApp/static/css/main.f8add6ea.chunk.css"
+  },
+  {
+    "revision": "0add9fed4024fb020d70",
+    "url": "/shadhinMusicApp/static/js/2.d0e7538b.chunk.js"
+  },
+  {
+    "revision": "bac08d190ad2c3782685257760a40a06",
+    "url": "/shadhinMusicApp/static/js/2.d0e7538b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05e532cf51e8458d118",
+    "url": "/shadhinMusicApp/static/js/main.36011ba1.chunk.js"
+  },
+  {
+    "revision": "ee459265d3de73000970",
+    "url": "/shadhinMusicApp/static/js/runtime-main.27be40d7.js"
+  },
+  {
+    "revision": "d9c23068ef426a94453f85a8c1a695b5",
+    "url": "/shadhinMusicApp/static/media/Feather.d9c23068.woff"
+  },
+  {
+    "revision": "fd51ad35733967464cf29e971da565ac",
+    "url": "/shadhinMusicApp/static/media/Feather.fd51ad35.svg"
+  },
+  {
+    "revision": "fe1594343a6aed9427c646993d06ea9c",
+    "url": "/shadhinMusicApp/static/media/Feather.fe159434.ttf"
+  },
+  {
+    "revision": "3fe8edbdf88a72e2f71263f77061b4c3",
+    "url": "/shadhinMusicApp/static/media/artist1.3fe8edbd.jpg"
+  },
+  {
+    "revision": "45c73723862c6fc5eb3d6961db2d71fb",
+    "url": "/shadhinMusicApp/static/media/fontawesome-webfont.45c73723.eot"
+  },
+  {
+    "revision": "4b5a84aaf1c9485e060c503a0ff8cadb",
+    "url": "/shadhinMusicApp/static/media/fontawesome-webfont.4b5a84aa.woff2"
+  },
+  {
+    "revision": "76a4f23c6be74fd309e0d0fd2c27a5de",
+    "url": "/shadhinMusicApp/static/media/fontawesome-webfont.76a4f23c.svg"
+  },
+  {
+    "revision": "7c87870ab40d63cfb8870c1f183f9939",
+    "url": "/shadhinMusicApp/static/media/fontawesome-webfont.7c87870a.ttf"
+  },
+  {
+    "revision": "dfb02f8f6d0cedc009ee5887cc68f1f3",
+    "url": "/shadhinMusicApp/static/media/fontawesome-webfont.dfb02f8f.woff"
+  },
+  {
+    "revision": "303ea1f41ab4813d952edf730b045305",
+    "url": "/shadhinMusicApp/static/media/logo2.303ea1f4.png"
+  },
+  {
+    "revision": "8b49eee8b6aa378f3e531d94ad64660b",
+    "url": "/shadhinMusicApp/static/media/notification1.8b49eee8.jpg"
+  },
+  {
+    "revision": "1daa4e3eb4a01c6c8a2cf06bf84d5d66",
+    "url": "/shadhinMusicApp/static/media/notification2.1daa4e3e.jpg"
+  },
+  {
+    "revision": "aa3549206622e14cb0f992826f91527e",
+    "url": "/shadhinMusicApp/static/media/notification3.aa354920.jpg"
+  },
+  {
+    "revision": "241236e7ad08027e90d9d44e5bf0fde1",
+    "url": "/shadhinMusicApp/static/media/subscription-header-bg.241236e7.jpg"
+  },
+  {
+    "revision": "65f702bbc66d094793072f8a72b1b55e",
+    "url": "/shadhinMusicApp/static/media/user.65f702bb.jpg"
+  }
+]);
